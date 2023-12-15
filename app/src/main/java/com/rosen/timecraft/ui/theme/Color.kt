95d1d3c812f36a96ae18625624f8daa96ef10b95
DerekWasswa <wasswadero@gmail.com>
2023-12-15 16:29:45 +0300

    add project colors

---
+++ app/src/main/java/com/rosen/timecraft/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.rosen.timecraft.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Black = Color(0xFF000714)
+val PriceBlack = Color(0xFF242527)
+val NameBlack = Color(0xFF363A3F)
+val CoffeeBrown = Color(0xFFDFB096)
+val White = Color(0xFFFFFFFF)
